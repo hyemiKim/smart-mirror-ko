bb16dbbb94ef2d9f65f14085ec02903a424e4771
soeun <mojosoeun@gmail.com>
2016-05-16 20:44:15 +0900

    change soudcloud api key variable name

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var FORCAST_API_KEY = "{}";
 var SUBWAY_API_KEY = "{}";
-var SOUNDCLOUD_APT_KEY = "{}";
+var SOUNDCLOUD_API_KEY = "{}";
 var YOUTUBE_API_KEY = "{}";
 var HUE_BASE = "http://192.168.0.36/api/{}/";
